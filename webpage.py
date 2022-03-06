7b449036a0fa549f9116ae15f73f99fcfac4c339
Paulo Kuriki <paulokuriki@gmail.com>
2022-03-06 17:38:06 -0300

    corrections

--- webpage.py
+++ webpage.py
@@ -10,17 +10,17 @@ import requests
 import constants as c
 
 st.set_page_config(
-     page_title="Report API Tester",
- )
+    page_title="Report API Tester",
+)
 
-def call_analyse(*args: tuple):
+
+def call_analyse(report: str):
     '''
     A callback function that receives the report text inside a tuple and makes the request to the API
 
-    :param args:
+    :param report text:
     :return None:
     '''
-    report = args[0]
 
     url = f"{c.API_HOST}:{c.API_PORT}/analyse_report"
     if 'http' not in url:
@@ -31,27 +31,34 @@ def call_analyse(*args: tuple):
     try:
         response = requests.post(url=url, json=json)
 
-        # prints the result in the page
         if 'ABNORMAL' in response.json():
+            # prints a yellow message
             st.warning(response.json())
         else:
+            # prints a green message
             st.success(response.json())
 
     except Exception as e:
         # prints the result in the page
         st.exception(f'{e}')
 
+
 # creates the HTML objects
 st.title('AI Report Classifier')
 st.header("Welcome to the NLP Report Classifier")
 st.write("This webpage was created using **Streamlit** to demonstrate how to request inferences from an API.")
 
-# input text box
-text = st.text_area('Enter your report text below then click the Analyse Report button.', '')
+with st.form("my_form"):
+    # input text box
+    text = st.text_area('Enter your report text below then click the Analyse Report button.', '')
 
-# analyze button
-st.button('Analyse Report', key='analyse', help="Click to analyse if the report is normal", on_click=call_analyse,
-             args=(text,))
+    # analyze button
+    submitted = st.form_submit_button('Analyse Report')
+
+
+if submitted:
+    st.subheader("Result")
+    call_analyse(text)
 
 st.write('')
 st.write('')
